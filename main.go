0723025b13ec42fa233e24960e6c8442dd9b9651
Sam Sullivan <samsullivan09@gmail.com>
2024-01-13 21:42:16 -0800

    cleanup unnecessarily verbose comments

--- main.go
+++ main.go
@@ -8,26 +8,24 @@ import (
 	"github.com/samsullivan/jqless/model"
 )
 
+// main starts the bubbletea program
 func main() {
-	// get file, expected to be JSON data
 	file, err := getFile()
 	if err != nil {
 		panic(err)
 	}
 
-	// create new model with JSON file
 	m, err := model.New(file)
 	if err != nil {
 		panic(err)
 	}
 
-	// start bubbletea program
 	if _, err := tea.NewProgram(m).Run(); err != nil {
 		panic(err)
 	}
 }
 
-// getFile opens the first command line argument or piped data.
+// getFile returns a file descriptor, expected to contain JSON data.
 func getFile() (file *os.File, err error) {
 	if len(os.Args) > 1 {
 		// if command line arguments included, attempt to open local file
